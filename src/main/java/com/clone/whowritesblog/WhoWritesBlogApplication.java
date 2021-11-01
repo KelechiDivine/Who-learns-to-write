6090f7809172368ef1c34c8e643d391eb33f1342
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-11-01 17:19:24 +0100

    set up the multi-module project

---
+++ src/main/java/com/clone/whowritesblog/WhoWritesBlogApplication.java
@@ -0,0 +1,13 @@
+package com.clone.whowritesblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoWritesBlogApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(WhoWritesBlogApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/clone/whowritesblog/WhoWritesBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.whowritesblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoWritesBlogApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
