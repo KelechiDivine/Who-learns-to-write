b1912fd588883d01c0ee6cea21d14de7b27daf92
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-11-03 00:17:40 +0100

    Succesfully connected to mySql

--- src/main/java/com/whowritesblog/model/Client.java
+++ src/main/java/com/whowritesblog/model/Client.java
@@ -1,4 +1,50 @@
 package com.whowritesblog.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Client {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Column(nullable = false)
+	private String firstname;
+	
+	@Column(nullable = false)
+	private String lastname;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Column(nullable = false, unique = true, length = 11)
+	private String phoneNumber;
+	
+	@Column(nullable = false)
+	private LocalDate accountCreationDate;
+	
+	@Column(nullable = false)
+	private String dateOfBirth;
+	
+	@Column(nullable = false)
+	@Transient
+	private Integer age;
+	
+	@Column(nullable = false)
+	private String address;
+	
+	@Column(nullable = false)
+	private String country;
+	
+	@Column(nullable = false)
+	private Boolean iVerified;
 }
