379e56b13f2272927594bf83d4fd1e74217840f7
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-11-03 14:33:54 +0100

    Performed the basic CRUD method in the client model repository test

--- src/main/java/com/whowritesblog/repository/ClientRepository.java
+++ src/main/java/com/whowritesblog/repository/ClientRepository.java
@@ -4,8 +4,14 @@ import com.whowritesblog.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
 	
+	default void updateClient(Client client){
+		client.setAccountCreationDate(LocalDate.now());
+		save(client);
+	}
 	
 }
--- src/test/java/com/whowritesblog/repository/ClientRepositoryTest.java
+++ src/test/java/com/whowritesblog/repository/ClientRepositoryTest.java
@@ -5,19 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.List;
 
 @SpringBootTest
 @Slf4j
@@ -44,32 +36,31 @@ public class ClientRepositoryTest {
 	@Test
 	public void test_createClient(){
 		
-		client.setFirstname("Iclass");
-		client.setLastname("Chima");
+		client.setFirstname("Ozioma");
+		client.setLastname("Okoroafor");
 		client.setAddress("peaceLand estate");
 		client.setCountry("Nigeria");
 		client.setAccountCreationDate(LocalDate.now());
 		client.setAge(23);
-		client.setPhoneNumber("08024002434");
-		client.setDateOfBirth("April 1");
-		client.setEmail("iclassChima@gmail.com");
+		client.setPhoneNumber("09099474849");
+		client.setDateOfBirth("February 27");
+		client.setEmail("oziomaOkoroafor@gmail.com");
 		client.setIsVerified(false);
 		
 		Assertions.assertThat(client).isNotNull();
 		
-		Assertions.assertThat(client).hasFieldOrPropertyWithValue("firstname", "Iclass");
-		Assertions.assertThat(client).hasFieldOrPropertyWithValue("lastname", "Chima");
+		Assertions.assertThat(client).hasFieldOrPropertyWithValue("firstname", "Ozioma");
+		Assertions.assertThat(client).hasFieldOrPropertyWithValue("lastname", "Okoroafor");
 		Assertions.assertThat(client).hasFieldOrPropertyWithValue("address", "peaceLand estate");
 		Assertions.assertThat(client).hasFieldOrPropertyWithValue("country", "Nigeria");
 		Assertions.assertThat(client).hasFieldOrPropertyWithValue("accountCreationDate", LocalDate.now());
 		Assertions.assertThat(client).hasFieldOrPropertyWithValue("age", 23);
-		Assertions.assertThat(client).hasFieldOrPropertyWithValue("phoneNumber", "08024002434");
-		Assertions.assertThat(client).hasFieldOrPropertyWithValue("dateOfBirth", "April 1");
-		Assertions.assertThat(client).hasFieldOrPropertyWithValue("email", "iclassChima@gmail.com");
+		Assertions.assertThat(client).hasFieldOrPropertyWithValue("phoneNumber", "09099474849");
+		Assertions.assertThat(client).hasFieldOrPropertyWithValue("dateOfBirth", "February 27");
+		Assertions.assertThat(client).hasFieldOrPropertyWithValue("email", "oziomaOkoroafor@gmail.com");
 		Assertions.assertThat(client).hasFieldOrPropertyWithValue("isVerified", false);
 		
-		clientRepository.save(client);
-		
+
 		log.info("Successfully created client with email --> {}", client.getEmail());
 		log.info("Successfully created client with phone number --> {}", client.getPhoneNumber());
 	}
@@ -81,5 +72,32 @@ public class ClientRepositoryTest {
 		log.info("Found client in database --> {}", client.getId());
 		log.info("Found client by first name --> {}", client.getFirstname());
 		log.info("Found client by email --> {}", client.getEmail());
+	}
+	
+	@Test
+	public void test_findAllClient(){
+		List<Client> clientRepositoryAll = clientRepository.findAll();
+		Assertions.assertThat(clientRepositoryAll).isNotNull();
+		log.info("Found all client in the database --> {}", clientRepository.findAll());
+	}
+	
+	@Test
+	public void test_deleteClientById(){
+		client = clientRepository.findById(8).orElse(null);
+		Assertions.assertThat(client).isNotNull();
+		clientRepository.deleteById(8);
+		log.info("Deleted client successfully --> {}", client.getId());
+	}
+	
+	@Test
+	public void test_updateClientById(){
+		client = clientRepository.findById(9).orElse(null);
+		Assertions.assertThat(client).isNotNull();
+		
+		client.setEmail("newClientUpdate@gmail.com");
+		client.setLastname("new client");
+		
+		clientRepository.updateClient(client);
+		log.info("Updated client successfully --> {}", client);
 	}
 }
