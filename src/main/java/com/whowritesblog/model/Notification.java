52637f4181d7ac879e1d504130ad6859ef1dbee6
Ozioma <oziomaokoroafor@gmail.com>
2021-11-07 14:04:12 +0100

    Wrote the notification model

--- src/main/java/com/whowritesblog/model/Notification.java
+++ src/main/java/com/whowritesblog/model/Notification.java
@@ -1,4 +1,21 @@
 package com.whowritesblog.model;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
 public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDate accountCreationDate;
+
+    @Column(nullable = false)
+    private Boolean hasBeenRead;
+
 }
