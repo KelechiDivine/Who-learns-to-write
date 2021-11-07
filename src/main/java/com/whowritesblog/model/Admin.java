8cda2423ea2ca701f8996c7b8d28c6dabf7a21ba
Ozioma <oziomaokoroafor@gmail.com>
2021-11-07 18:49:01 +0100

    Admin model added

--- src/main/java/com/whowritesblog/model/Admin.java
+++ src/main/java/com/whowritesblog/model/Admin.java
@@ -1,4 +1,33 @@
 package com.whowritesblog.model;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+
 public class Admin {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String dateOfBirth;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column (nullable = false)
+    private String phoneNumber;
+
 }
